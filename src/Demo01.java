cecb162bd8fc56d0f8f6edd0ef903a3afb4b8208
lipengfei.li <lipengfei.li@qunar.com>
2016-11-03 11:52:49 +0800

    demo01_for

--- src/Demo01.java
+++ src/Demo01.java
@@ -1,6 +1,15 @@
 /**
- * Created by Administrator on 2016-11-03.
+ * Created by IDEA
+ * User: lipengfei.li
+ * Date: 16-11-3
+ * Time: 上午11:36
+ * To change this：ActionScript File Header
  */
 public class Demo01 {
-
+    public static void main(String[] args) {
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,8 +5,9 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello World");
-        System.out.println("git push");
+        System.out.println("git push");//测试
         System.out.println("git pull");
+
 
     }
 }
