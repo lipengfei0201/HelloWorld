2fc78758f33f245d4b380184162e3260e6182604
lipengfei.li <lipengfei.li@qunar.com>
2017-05-08 20:05:08 +0800

    pro

--- src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lipengfei.li on 2016-11-03.
- * 参见前端HelloWorld.html ，运行改程序会弹出Applet小程序
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
